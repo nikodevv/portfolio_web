1e1e8f169f35f10cf0830bb6d438af19848b2547
Niko Oshinov <niko.oshinov@gmail.com>
2018-01-13 17:42:47 -0500

    Added home page and test for home_page view

--- home_pages/tests.py
+++ home_pages/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestViews(TestCase):
+	def test_homepage_returns_correct_html(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'homepage.html')
--- portfolio_web/urls.py
+++ portfolio_web/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from home_pages import urls as homepage_urls
+from home_pages import views as homepage_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^/home_pages/', include(homepage_urls)),
+    url(r'^$', homepage_views.home_view, name='homepage')
 ]
