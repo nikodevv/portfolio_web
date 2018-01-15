f9e5b08bc56ae9c195c8bdd2f90e1196027a2b74
Niko Oshinov <niko.oshinov@gmail.com>
2018-01-14 21:53:44 -0500

    finally styled navbar in a likable fashion

--- home_pages/views.py
+++ home_pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-	return render(request, 'homepage.html')
+	return render(request, 'homepage.html', {'current_page': 'Home'})
 
 def contact_view(request):
-	return render(request, 'contact.html')
+	return render(request, 'contact.html', {'current_page': 'Contact'})
