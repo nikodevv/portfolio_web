ae6aabbf700920d93cbb0e97d5e20a5a18e9913e
Niko Oshinov <niko.oshinov@gmail.com>
2018-03-12 18:11:34 -0400

    Added and passed test for filtering of tournament metadata in Procssor

--- stats/database_tools/Controller.py
+++ stats/database_tools/Controller.py
@@ -4,42 +4,52 @@ from stats import models # might cause issues
 from django.db import models
 from stats.models import Tournament, Match
 
+
 class Controller:
+	"""
+	Makes API calls and dispatches information to be Processed
+	"""
 	START_ID = 17411 # using itemdef not id
 	END_ID = 25000 # using itemdef not id
-
+	
 	def __init__(self, API_KEY):
-		# To-Do: import Models Base Class
 		self.api = dota2api.Initialise(API_KEY)
-		self.processor = Processor()
+		self.processor = Processor(Controller.START_ID, Controller.END_ID)
 
-	def create_tournaments(self, tournament_data=None):
-		# allows unit testing
-		if tournament_data == None:
-			tournament_data = self.get_tournaments(START, END)
+	def create_tournaments(self):
+		"""Collects data from valve API and passes it to processor"""
+		self.processor.create_tournaments(self.get_tournaments(START_ID, END_ID), BasicAPI())
 
 	def get_tournaments(self, start, end):
-		"""returns array with tournament metadata"""
-		wrapper_leagues = self.api.get_league_listing()
-		tournaments = []
-		for tourmnt in wrapper_leagues['leagues']:
-			if (tourmnt['itemdef'] <= end) and (tourmnt['itemdef'] >= start):
-				try:
-					tournaments.append({'index': tourmnt['itemdef'], 
-						'id': tourmnt['leagueid'], 'name': tourmnt['name']})
-				except:
-					# Likely Encoding error
-					pass
-		return tournaments
+		"""calls api to find necessary tournaments"""
+		# for tourmnt in leagues:
+		# 	self.processor.create_tournament()
+			#if (tourmnt['itemdef'] <= end) and (tourmnt['itemdef'] >= start):
+
+		return self.api.get_league_listing()['leagues']
+
+
+	def get_match_details(self, match_id):
+		#self.api.
+		pass
 
 class Processor:
-	""" A data processing layer that passes input to models.Manager """
-	def __init__(self):
-		t_manager = TournamentManager()
-		g_manager = GameManager()
+	""" A data processing layer that takes large dictonaries, and filters 
+	the data to models.Managers (models factories) """ 
+	def __init__(self, start, end):
+		self.t_manager = TournamentManager()
+		self.g_manager = GameManager()
+		self.START_ID = start
+		self.END_ID = end
 
-	def create_tournament(tournament_data):
-		t_manager.create() # not defined yet
+	def create_tournaments(tournament_data):
+		tournaments = []
+		for data in tournament_data:
+			self.t_manager.create(self._filter_tdata(data))
+
+	@staticmethod
+	def _filter_tdata(data):
+		return data['leagueid'], data['itemdef'], data['name']
 
 class TournamentManager(models.Manager):
 	@staticmethod
@@ -52,11 +62,14 @@ class TournamentManager(models.Manager):
 		return entry
 
 class GameManager(models.Manager):
-	def create(self, tournament, match, win_r, rad_teamid, dire_teamid, hero_ids, player_ids):
-		self.create_match(tournament, match, win_r, rad_teamid, dire_teamid, hero_ids, player_ids)
+	def create(self, tournament, match, win_r, rad_teamid, dire_teamid, 
+		hero_ids, player_ids):
+		self.create_match(tournament, match, win_r, rad_teamid, dire_teamid, 
+			hero_ids, player_ids)
 
 	@staticmethod
-	def create_match(tournament, match_id, win_r, rad_teamid, dire_teamid, hero_ids, player_ids):
+	def create_match(tournament, match_id, win_r, rad_teamid, dire_teamid, 
+		hero_ids, player_ids):
 		entry = Match()
 		entry.mid = match_id
 		entry.win_radiant = win_r
--- stats/tests/test_dataGeneration.py
+++ stats/tests/test_dataGeneration.py
@@ -19,3 +19,14 @@ class TestController(PythonTestCase):
 		default =  ({'status': 1, 'num_results': 0, 'total_results': 0, 
 			'results_remaining': 0, 'matches': []})
 		self.assertEqual(self.controller.api.get_match_history(league_id=1), default)
+
+class TestProcessor(PythonTestCase):
+	def setUp(self):
+		self.processor = Processor(17411, 25000)
+
+	def test_tournament_filter(self):
+		data = {'name': 'one_name', 'randomename': 'randomVar', 'itemdef':3, 'leagueid':30}
+		arg1, arg2, arg3 = self.processor._filter_tdata(data)
+		self.assertEqual(arg1, data['leagueid'])
+		self.assertEqual(arg2, data['itemdef'])
+		self.assertEqual(arg3, data['name'][:20])
