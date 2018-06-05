0e936cf9e9bd2626db79149bd6bdeda72a5b964a
nikodevv <niko.oshinov@gmail.com>
2018-06-05 18:48:32 -0400

    can filter based on team_ids, inclusively

--- stats/views.py
+++ stats/views.py
@@ -30,7 +30,11 @@ class TournamentList(APIView):
 
 class MatchList(ListAPIView):
 	"""
-	Displays list of matches based on query parameters
+	Displays list of matches based on query parameters.
+	To link together multiple parameters for the same 
+	field (i.e. to look up two different team ids),
+	seperate parameters with '-'. For example
+	./matches?team_id=11111-22222
 	"""
 	serializer_class = MatchSerializer
 
@@ -42,5 +46,28 @@ class MatchList(ListAPIView):
 		queryset =  Match.objects.all()
 		team_id = self.request.query_params.get('team_id', None)
 		if team_id is not None:
-			queryset = queryset.filter(rad_teamid=team_id)
+			team_id = self._format_team_id(team_id)
+			queryset = self._filter_teams(queryset, team_id)
 		return queryset
+
+	def _filter_teams(self, queryset, teams):
+		# Note time complexity does not include postgreSQL
+		# search operations (generally log(n) time).
+		print("fisrt function is called")
+		# sets queryset to match any games with first team in teams
+		final_queryset = self._filter_for_team_id(queryset, teams[0])
+		# unites querysets for each team to the first
+		for team in teams:
+			if team is not teams[0]:
+				final_queryset = final_queryset.union(
+					self._filter_for_team_id(queryset, team))
+		return final_queryset
+		
+	def _filter_for_team_id(self, queryset, team_id):
+		return queryset.filter(rad_teamid=team_id).union(
+			queryset.filter(dire_teamid=team_id))
+
+	def _format_team_id(self, team_ids_string):
+		if "-" not in team_ids_string:
+			return [team_ids_string]
+		return team_ids_string.split("-")
