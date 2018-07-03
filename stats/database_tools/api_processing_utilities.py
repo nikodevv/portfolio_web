a9a84b1c8944e7b2a669ac34e4155d109ca0a94f
nikodevv <niko.oshinov@gmail.com>
2018-07-02 22:56:59 -0400

    Added detail to error logging, moved error log file to .gitignore

--- stats/database_tools/api_processing_utilities.py
+++ stats/database_tools/api_processing_utilities.py
@@ -2,15 +2,34 @@
 A collection of utilities for the processing of Valve API data
 """
 from django.db import IntegrityError
+from time import time, ctime
+import pickle
 
 def ignore_duplicate_data_error(fn):
 	"""
 	Decorator that prevents IntegrityError[s] causing 
 	crashes during model validation.
 	"""
-	def wrapper(*args):
+	def get_args_dict(*args, **kwargs):
+		"""
+		Serializes an arbitrary number of kwargs into a str
+		"""
+		# utf-8 will have issues with valve api
+		return pickle.dumps(kwargs).decode('utf-16')
+
+	def wrapper(*args, **kwargs):
 		try:
 			fn(*args)
-		except IntegrityError:
-			print("IntegrityError: Duplicate data. Data not saved to database")
+		except IntegrityError as e:
+			log_str = (ctime(time()) + 
+				" - prevented IntegrityError crash by call " + fn.__name__ + 
+				": Duplicate data. Data not saved to database." + 
+				"\r\narguments:::: " + get_args_dict(kwargs))
+			add_to_log(log_str)
 	return wrapper
+
+def add_to_log(data):
+		with open('errorLog.txt', 'ab') as log:
+			# Python automatically uses correct line endings
+			# to insert new line via \n
+			log.write(('\r\n%s' % data).encode())
