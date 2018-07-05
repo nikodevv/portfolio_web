146e30fa36938ec96dab8d5fd4d8adc511435248
nikodevv <niko.oshinov@gmail.com>
2018-07-05 18:43:46 -0400

    Refactored models so they are normalized

--- stats/models.py
+++ stats/models.py
@@ -6,7 +6,7 @@ class Tournament(models.Model):
 	""" 
 	Class representing a pdro tournament metadata
 	"""
-	tid = models.CharField(max_length=255, primary_key=True, null=False)
+	tid = models.CharField(max_length=255, primary_key=True)
 	tindex = models.CharField(max_length=255, default='novalue')
 	tname = models.CharField(max_length=255, default='novalue')
 
@@ -14,10 +14,10 @@ class Tournament(models.Model):
 class Match(models.Model):
 	# ids are CharFields to prevent reading 003 as 3.
 	# Foreign Keys
-	tournament = models.ForeignKey(Tournament, null=False, blank=True)
+	tournament = models.ForeignKey(Tournament, blank=True)
 	# Game attributes
 	mid = models.CharField(primary_key=True, max_length=255) # match id
-	mlength = models.CharField(max_length=255) # match length
+	# mlength = models.CharField(max_length=255) # match length
 	win_radiant = models.BooleanField(default=True)
 	# Team specific attributes
 	rad_teamid = models.CharField(max_length=255, default='novalue')
@@ -27,11 +27,10 @@ class Player(models.Model):
 	"""
 	Holds match-specifc player info
 	"""
-	match = models.ForeignKey(Match, null=False, blank=True)
+	match = models.ForeignKey(Match, on_delete=models.CASCADE)
 	# True or False whether Player is on radiant side.
 	rad = models.BooleanField()
 	heroid = models.CharField(max_length=255, default='novalue')
 	playerid = models.CharField(max_length=255, default='novalue')
-
 	class Meta:
 		unique_together = (('match', 'playerid'), ('match', 'heroid'))
