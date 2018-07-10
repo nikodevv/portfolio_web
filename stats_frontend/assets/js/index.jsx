fb1706961ebc591e04478e5cea89879370143e78
nikodevv <niko.oshinov@gmail.com>
2018-07-09 20:33:14 -0400

    Full react dev setup

---
+++ stats_frontend/assets/js/index.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+    render(){
+        return (
+            <h1>
+            Hello, React!
+            </h1>
+        );
+    }
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'))
