ddf73f5d36254c8fabbd2bd0ace5b423eaadef61
nikodevv <niko.oshinov@gmail.com>
2018-07-11 01:33:49 -0400

    Added imports to store

--- stats_frontend/assets/js/reduxStore.js
+++ stats_frontend/assets/js/reduxStore.js
@@ -1,3 +1,12 @@
 import {createStore} from 'redux';
-import heroSearchApp from './reducers';
+import {heroSearchApp} from './reducers';
+import {
+    offToggleRelevantHero,
+    onToggleRelevantHero,
+    extendHeroSearchStr,
+    reduceHeroSearchStr,
+    deleteHeroSearchStr
+} from './reducers';
 const store = createStore(heroSearchApp);
+//log initial state
+console.log(store.getState())
